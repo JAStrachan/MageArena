43cdc47bc46a604a180457f40fc03144aac00621
JAStrachan <jstrachansss@gmail.com>
2018-04-17 00:07:45 +0000

    Initial Commit, Basic Camera Implemented as well

---
+++ MageArena/Source/MageArena/Public/MageMesh.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "MageMesh.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGEARENA_API UMageMesh : public USkeletalMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
